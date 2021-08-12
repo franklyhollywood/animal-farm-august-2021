d112665d02e52f70a20614caf31c1a3cad5b7f47
Frank Hollywood <github@srvsync.com>
2021-08-11 17:27:40 -0700

    Added sounds to click and animation of images on hover

--- app.js
+++ app.js
@@ -2,7 +2,26 @@
 
 // initialize state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// set event listeners
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
+
+const catSound = document.getElementById('catSound');
+const catImage = document.getElementById('cat');
+const dogSound = document.getElementById('dogSound');
+const dogImage = document.getElementById('dog');
+const horseSound = document.getElementById('horseSound');
+const horseImage = document.getElementById('horse');
+
+dogImage.addEventListener('click', () => {
+	dogSound.play();
+});
+
+catImage.addEventListener('click', () => {
+	catSound.play();
+});
+
+horseImage.addEventListener('click', () => {
+	horseSound.play();
+});
